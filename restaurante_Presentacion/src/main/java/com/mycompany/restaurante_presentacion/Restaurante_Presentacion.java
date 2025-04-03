9b2683e6b21e62a8ba6f4afe561f7ea34f1c659d
TheleyendX <jorge.enriquez233274@potros.itson.edu.mx>
2025-04-02 18:20:16 -0700

    Creacion de proyectos
    Se crearon 3 proyectos que son las 3 capas Negocio, Persistencia y Presentacion

---
+++ restaurante_Presentacion/src/main/java/com/mycompany/restaurante_presentacion/Restaurante_Presentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.restaurante_presentacion;
+
+/**
+ *
+ * @author jorge
+ */
+public class Restaurante_Presentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
