7af2f9ab21498bc46636fc332c5b712b6b49866a
katianavarez <katia.navarez252855@potros.itson.edu.mx>
2025-04-06 22:04:23 -0700

    prueba extra

---
+++ restaurante_Dominio/src/main/java/Encriptador/Encriptador.java
@@ -0,0 +1,32 @@
+package Encriptador;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class Encriptador {
+
+    private static final String ALGORITHM = "AES";
+    private static final String SECRET_KEY = "1234567890123456"; // 16-byte secret key, puedes cambiarla.
+
+    // Encriptar el texto
+    public static String encrypt(String text) throws Exception {
+        SecretKeySpec keySpec = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+        byte[] encryptedBytes = cipher.doFinal(text.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    // Desencriptar el texto
+    public static String decrypt(String encryptedText) throws Exception {
+        SecretKeySpec keySpec = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, keySpec);
+        byte[] decodedBytes = Base64.getDecoder().decode(encryptedText);
+        byte[] decryptedBytes = cipher.doFinal(decodedBytes);
+        return new String(decryptedBytes);
+    }
+}
+
